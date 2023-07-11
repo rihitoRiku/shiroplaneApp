23197aac298396c023b6ca98e289cb292859f27f
slvally <fieshidiq@gmail.com>
2023-07-12 01:16:17 +0700

    try react-dropzone

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,33 +1,47 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useDropzone } from "react-dropzone";
 import Axios from "axios";
 
 function Dasboard() {
   const [imageSelected, setImageSelected] = useState("");
-  const UploadImage = (files) => {
-    console.log(files[0]);
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const UploadImage = () => {
+    console.log(acceptedFiles[0]);
     const formData = new FormData();
-    formData.append("file", imageSelected);
+    formData.append("file", acceptedFiles[0]);
     formData.append("upload_preset", "er4tbb4l");
 
     Axios.post(
       "https://api.cloudinary.com/v1_1/dqmorrdhr/upload",
       formData
     ).then((Response) => {
-      console.log(Response); 
+      console.log(Response);
     });
   };
+
+  const files = acceptedFiles.map((file) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
   return (
     <>
-      <div className="flex items-center justify-center w-full h-screen border-2">
-        <input
-          className=""
-          type="file"
-          onChange={(event) => {
-            setImageSelected(event.target.files[0]);
-          }}
-        />
-        <button onClick={UploadImage}>Upload image</button>
+      <div className="flex flex-col items-center justify-center w-full h-48 border-2">
+        <div className=" border-2">
+          <div {...getRootProps({ className: "dropzone" })}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          </div>
+        </div>
+        <div className=" border-2">
+          <button onClick={UploadImage}>Upload image</button>
+        </div>
+        <aside>
+          <h4>Files</h4>
+          <ul>{files}</ul>
+        </aside>
       </div>
     </>
   );
