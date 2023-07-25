713fb69851d1b98257d83719d0a1e0fb5d367fdf
slvally <fieshidiq@gmail.com>
2023-07-25 20:45:08 +0700

    details commit

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,25 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import "./Detail.css";
-// import p1 from "../../assets/p4.jpg";
+import Loader from "../../../src/components/loader/loader";
+import Axios from "axios";
 
 function Detail() {
+  const { id } = useParams();
+  console.log(id);
+  const [dataImages, setDataImages] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await Axios.get(`https://shiroplane-backend.vercel.app/images`).then(
+          (response) => {
+            setDataImages(response.data.data[id]);
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [id]);
   return (
     <>
-      <div className="mb-5 max-w-7xl">
+      <div className="mb-5 ms-2 mt-2 max-w-7xl">
         <div className="mb-2 text-3xl md:text-4xl font-normal text-gray-800">
-          Burritos
+          {dataImages.title}
         </div>
         <div className="text-md md:text-xl text-gray-600">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet,
-          nostrum numquam. Esse quaerat soluta magni nam atque beatae illo
-          blanditiis. amet consectetur adipisicing elit. Amet, nostrum numquam.
-          Esse quaerat soluta magni nam atque beatae illo blanditiis
+          {dataImages.desc}
         </div>
       </div>
       <div className="w-[84rem]">
         <div className="">
-          {/* <img className="w-full" src={p1} alt="" /> */}
+          <img className="w-full" src={dataImages.imgSrc} alt="" />
         </div>
       </div>
     </>
